2fd960af5f2856c3d6cfbe317369c01e0c566d4b
reka prihatanto <rey.barrolz@gmail.com>
2015-03-25 15:35:28 +0700

    edit layout api call

--- resources/views/konten/backend/api_call/list_data.blade.php
+++ resources/views/konten/backend/api_call/list_data.blade.php
@@ -12,7 +12,7 @@
 		<?php $no= $api_call->firstItem(); ?>
 @foreach($api_call as $list)
 		<tr>
-			<td>{!! $no !!}</td>
+			<td class="text-center">{!! $no !!}</td>
 			<td>{!! $list->nama !!}</td>
 			<td> @if(count($list->mst_user)>0) {!! $list->mst_user->nama !!} @else - @endif </td>
 			<td>{!! Fungsi::date_to_tgl(date('Y-m-d', strtotime($list->created_at))) !!}/ {!! date('H:i:s', strtotime($list->created_at)) !!}</td>
