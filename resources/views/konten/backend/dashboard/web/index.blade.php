519f489891f63fb46dfd8b01d43f68e92f8db15b
reka prihatanto <rey.dbarrolz@gmail.com>
2015-03-28 05:52:42 +0700

    add dashboard level web

--- resources/views/konten/backend/dashboard/web/index.blade.php
+++ resources/views/konten/backend/dashboard/web/index.blade.php
@@ -7,25 +7,8 @@
 
 @section('konten_utama')
 
-  @include($base_view.'list_data')
+  @include($base_view.'web.list_data')
 
-  <div class="col-lg-3 col-xs-6 animated fadeIn">
-      <div class="small-box bg-olive">
-        <div class="inner">
-            <h3> 
-               0
-            </h3>
-            <p>
-                Jml Arsip
-            </p>
-        </div>
-        <div class="icon">
-           <i class='fa fa-archive'></i>
-        </div>
-        <a href="http://somelier.reka.com/my_archive" class="small-box-footer">
-            More info <i class="fa fa-arrow-circle-right"></i>
-        </a>
-    </div>
 
 
 
