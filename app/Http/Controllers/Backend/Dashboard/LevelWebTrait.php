aa87d9dcf0f3ac0b622746f7b51ed13926c4e2e5
reka prihatanto <rey.barrolz@gmail.com>
2015-03-28 09:07:11 +0700

    tambah info dashboard level web

--- app/Http/Controllers/Backend/Dashboard/LevelWebTrait.php
+++ app/Http/Controllers/Backend/Dashboard/LevelWebTrait.php
@@ -1,6 +1,9 @@
 <?php namespace App\Http\Controllers\Backend\Dashboard;
 
 use App\Models\Mst\Berita;
+use App\Models\Mst\FotoSlide;
+use App\Models\Mst\SlideUtama;
+use App\Models\Mst\Weblink;
 
  
 
@@ -11,7 +14,11 @@ trait LevelWebTrait{
  
 	public function level_web(){
 		$jml_berita = Berita::count();
-		return view('konten.backend.dashboard.web.index', compact('jml_berita'));
+		$jml_weblink = Weblink::count();
+		$jml_foto_slide = FotoSlide::count();
+		$jml_foto_slide_utama = SlideUtama::count();
+		return view('konten.backend.dashboard.web.index', 
+			compact('jml_berita', 'jml_weblink', 'jml_foto_slide', 'jml_foto_slide_utama'));
 	}
 
 
--- resources/views/konten/backend/dashboard/web/list_data.blade.php
+++ resources/views/konten/backend/dashboard/web/list_data.blade.php
@@ -16,3 +16,68 @@
         </a>
     </div>
 </div>
+
+
+  <div class="col-lg-3 col-xs-6 ">
+      <div class="small-box bg-purple">
+        <div class="inner">
+            <h3> 
+               {!! $jml_weblink !!}
+            </h3>
+            <h4>
+               Jml Weblink
+            </h4>
+        </div>
+        <div class="icon">
+           <i class='fa fa-link'></i>
+        </div>
+        <a href="{{ URL::route('admin_weblink.index') }}" class="small-box-footer">
+            More info <i class="fa fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+
+
+
+
+
+  <div class="col-lg-3 col-xs-6 ">
+      <div class="small-box bg-yellow">
+        <div class="inner">
+            <h3> 
+               {!! $jml_foto_slide !!}
+            </h3>
+            <h4>
+               Jml Foto Slide
+            </h4>
+        </div>
+        <div class="icon">
+           <i class='fa fa-image'></i>
+        </div>
+        <a href="{{ URL::route('admin_weblink.index') }}" class="small-box-footer">
+            More info <i class="fa fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
+
+
+
+
+  <div class="col-lg-3 col-xs-6 ">
+      <div class="small-box bg-blue">
+        <div class="inner">
+            <h3> 
+               {!! $jml_foto_slide_utama !!}
+            </h3>
+            <h4>
+               Jml Foto Slide Utama
+            </h4>
+        </div>
+        <div class="icon">
+           <i class='fa fa-image'></i>
+        </div>
+        <a href="{{ URL::route('foto_slide_utama.index') }}" class="small-box-footer">
+            More info <i class="fa fa-arrow-circle-right"></i>
+        </a>
+    </div>
+</div>
