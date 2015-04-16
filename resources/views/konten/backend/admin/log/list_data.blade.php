5fa4f243986d541533b8e030e19c7abda0e72574
reka prihatanto <rey.barrolz@gmail.com>
2015-04-16 08:05:55 +0700

    tambah tgl+jam di menu logs

--- resources/views/konten/backend/admin/log/list_data.blade.php
+++ resources/views/konten/backend/admin/log/list_data.blade.php
@@ -1,9 +1,11 @@
 <table class="table table-bordered table-hover">
 	<thead>
 		<tr>
-			<th width="5%">No.</th>
+			<th width="5%" class="text-center">No.</th>
 			<th width="15%">User</th>
 			<th>Log</th>
+			<th>Tgl</th>
+			<th>Jam</th>
 
 		</tr>
 	</thead>
@@ -11,9 +13,11 @@
 	<?php $no = $log->firstItem(); ?>
 @foreach($log as $list)
 		<tr>
-			<td>{{ $no }}</td>
+			<td class="text-center">{{ $no }}</td>
 			<td> @if(count($list->mst_user)>0) {!! $list->mst_user->nama !!} @else - @endif </td>
 			<td> {!! $list->log !!} </td>
+			<td>{!! Fungsi::date_to_tgl(date("Y-m-d", strtotime($list->created_at))) !!}</td>
+			<td>{!! date("H:i:s", strtotime($list->created_at)) !!} WIB</td>
 
 		</tr>
 		<?php $no++; ?>
